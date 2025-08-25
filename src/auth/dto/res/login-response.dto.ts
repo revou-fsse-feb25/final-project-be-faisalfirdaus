b05acd073ee634d6d4fd21b7659e6df42c8abc34
Faisal Firdaus <faisal.firdaus10.ff@gmail.com>
2025-08-25 11:50:43 +0700

    chore: change env

--- src/auth/dto/res/login-response.dto.ts
+++ src/auth/dto/res/login-response.dto.ts
@@ -1,16 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class LoginResponseDto {
-  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6…' })
-  access_token: string;
-
-  @ApiProperty({ example: 'def50200b1c9e3f3fbc1…' })
-  refresh_token: string;
-
-  @ApiProperty({ type: () => UserLoginInfoDto })
-  user: UserLoginInfoDto;
-}
-
 export class UserLoginInfoDto {
   @ApiProperty({ example: 1 })
   id: number;
@@ -29,4 +18,15 @@ export class UserLoginInfoDto {
 
   @ApiProperty({ example: '2025-08-21T13:00:00.000Z' })
   created_at: Date;
+}
+
+export class LoginResponseDto {
+  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6…' })
+  access_token: string;
+
+  @ApiProperty({ example: 'def50200b1c9e3f3fbc1…' })
+  refresh_token: string;
+
+  @ApiProperty({ type: () => UserLoginInfoDto })
+  user: UserLoginInfoDto;
 }
